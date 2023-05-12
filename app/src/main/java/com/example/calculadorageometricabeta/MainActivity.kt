bc28197bbd5a95afa1e8e5196300ca68242c724e
AASG <betoxd2003@gmail.com>
2023-05-12 11:49:49 -0400

    Agregando cambios visuales y arreglos de la interfaz

--- app/src/main/java/com/example/calculadorageometricabeta/MainActivity.kt
+++ app/src/main/java/com/example/calculadorageometricabeta/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.sql.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayShowTitleEnabled(false);
 
         val ingresar = findViewById<Button>(R.id.btnIngresar);
 
